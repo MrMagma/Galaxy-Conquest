d218ba51de524ba77c235c4607796eb6aa323e72
Mr. Magma <joshuamagma2013@gmail.com>
2015-11-25 15:36:57 -0600

    Begin implementing Mars strategy game framework

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,58 +1,33 @@
-(function(fps) {
+(function() {
+    var FPS = 60;
+    var FRAME_INTERVAL = 1000 / FPS;
+    
+    var renderer = PIXI.autoDetectRenderer(
+        window.innerWidth,
+        window.innerHeight, {});
+    
+    var frameCount = 0;
+    
+    function runUpdate() {
+        var now = Date.now();
+        if (now - runUpdate.lastUpdate >= FRAME_INTERVAL) {
+            update();
+            frameCount += 1;
+        }
+        window.requestAnimationFrame(runUpdate);
+    };
+    
+    runUpdate.lastUpdate = -Infinity;
+    
+    function init() {
+        document.body.appendChild(renderer.view);
+        window.requestAnimationFrame(runUpdate);
+    };
+    
+    function update() {
+        console.log("Hi");
+    };
 
-	var msDelay = 1000 / fps;
+    document.addEventListener("DOMContentLoaded", init);
 
-	console.log("Delay between frames is: " + msDelay + " milliseconds");
-
-	var stars = [];
-
-	var canvas;
-
-	function init() {
-		var loader = new Messenger();
-		loader.addResource("text", "Messenger", "Scripts/Messenger.js");
-		loader.load(function() {
-			console.log(loader.resources.Messenger);
-		});
-		canvas = new Sketch({width: window.innerWidth, height: window.innerHeight});
-
-		var start = Date.now();
-
-		/* STRESS TEST */
-		var numObjects = 1000;
-		canvas.setShadow({enabled: true, hue: 360, saturation: 50, lightness: 50, blur: 20})
-		canvas.setStroke({hue: 0, saturation: 50, lightness: 50, weight: 1});
-		canvas.setFill({hue: 100, saturation: 50, lightness: 50});
-		canvas.setText({enabled: true, size: 20, font: "cursive"});
-		for (var i = 0; i < numObjects; i ++) {
-			var text = new Sketch.Text({
-				x: Math.random() * canvas.width,
-				y: Math.random() * canvas.height,
-				text: "The pen is mightier than the sword"
-			});
-			Math.sin(Math.random());
-			canvas.addChild(text);
-		}
-
-		canvas.render();
-
-		var end = Date.now();
-
-		console.log((end - start) + " milliseconds taken to add and render " + numObjects + " graphics objects");
-
-		setTimeout(update, msDelay);
-	}
-
-
-
-	function update() {
-
-		//canvas.render();
-
-
-		setTimeout(update, msDelay);
-	}
-
-	window.addEventListener("load", init, false);
-
-})(60);
+})();
