5e9448f061bf6bee09819cbdf633282c359dfd0e
Mr. Magma <joshuamagma2013@gmail.com>
2015-11-26 15:14:26 -0600

    Work on Graph

--- Mars/src/Graph.js
+++ Mars/src/Graph.js
@@ -1,50 +1,91 @@
 var Graph = (function() {
     
-    function Graph(cfg) {
-        
+    class GraphObject {
+        constructor({data = {}, listen = {}, verify = {}, change = {}}) {
+            this._listeners = {};
+            this._data = {};
+            this._checks = {};
+            this._changeListeners = {};
+            
+            for (let evt in listen) {
+                if (listen.hasOwnProperty(evt)) {
+                    this.on(evt, listen[evt]);
+                }
+            }
+            this.data(data);
+            this.verify(verify);
+            this.change(change);
+        }
+        data(dat) {
+            this._data = _.extendOwn(this._data, dat);
+            return this;
+        }
+        on(evt, callback = () => {}) {
+            if (typeof this._listeners[evt] === "undefined" ||
+                window.isNaN(this.listeners[evt]) ||
+                this._listeners[evt].constructor !== Array) {
+                this.listeners[evt] = [];
+            }
+            
+            if (callback.constructor === Function) {
+                this._listeners[evt].push(callback)
+            } else if(callback.constructor === Array) {
+                this._listeners[evt].push.apply(this._listeners[evt],
+                    callback);
+            }
+            
+            return this;
+        }
+        fire(evt, data = {}) {
+            if (typeof this.listeners[evt] !== "undefined") {
+                for (let listener of this.listeners[evt]) {
+                    listener(data);
+                }
+            }
+            
+            return this;
+        }
+        change(listeners) {
+            _.mapObject(listeners, function(value, key) {
+                
+            });
+            return this;
+        }
+        verify(checks) {
+            _.mapObject(checks, function(value, key) {
+                
+            });
+            return this;
+        }
     }
     
-    Graph.prototype = {
-        data() {
+    class Graph extends GraphObject {
+        constructor(cfg) {
             
         }
-    };
-    
-    
-    
-    function Node(data) {
-        
     }
     
-    Node.prototype = {
-        data() {
-            
-        },
-        change() {
+    
+    
+    class Node extends GraphObject {
+        constructor() {
             
         }
-    };
-    
-    
-    
-    function Edge(connections, data) {
-        
     }
     
-    Edge.prototype = {
-        data(data) {
-            
-        },
-        change() {
-            
-        },
-        connect(node) {
-            
-        },
-        unconnect(node) {
+    
+    
+    class Edge extends GraphObject {
+        constructor() {
             
         }
-    };
+        connect() {
+            
+        }
+        unconnect() {
+            
+        }
+    }
     
     
     
