e316cabbea10cdeea83b458341af9e8d848c3f51
Mr. Magma <joshuamagma2013@gmail.com>
2015-11-27 12:51:04 -0600

    Implement 'off' method

--- Mars/src/Graph.js
+++ Mars/src/Graph.js
@@ -1,6 +1,12 @@
 var Graph = (function() {
     
-    var _ = require("underscore");
+    let _ = require("underscore");
+    let UIDGenerator = require("./UIDGenerator");
+    
+    const CALLBACK_UID_KEY = "_mars_graph_callback_uid";
+    let uidGenerator = new UIDGenerator({
+        sequenceLength: 30
+    });
     
     class GraphObject {
         constructor({data = {}, listen = {}, verify = {}, change = {}}) {
@@ -22,11 +28,24 @@ var Graph = (function() {
             this._data = _.extendOwn(this._data, dat);
             return this;
         }
+        _callbackify(callback) {
+            if (typeof callback === "function") {
+                if (!callback[CALLBACK_UID_KEY]) {
+                    callback[CALLBACK_UID_KEY] = uidGenerator.generate();
+                }
+            } else if(callback.constructor === Array) {
+                callback.map(fn => {
+                    if (!fn[CALLBACK_UID_KEY]) {
+                        fn[CALLBACK_UID_KEY] = uidGenerator.generate();
+                    }
+                })
+            }
+        }
         on(...args) {
             if (args.length === 1) {
                 return this._onJSON.apply(this, args);
             }
-            if (args.length === 2) {
+            if (args.length >= 2) {
                 return this._onEvtCallback.apply(this, args);
             }
             return this;
@@ -55,25 +74,65 @@ var Graph = (function() {
                 this._listeners[evt] = [];
             }
             
-            if (callback.constructor === Function) {
-                this._listeners[evt].push(callback)
+            if (typeof callback === "function") {
+                callback[CALLBACK_UID_KEY] = uidGenerator.generate();
+                this._listeners[evt].push(callback);
             } else if(callback.constructor === Array) {
-                this._listeners[evt].push.apply(this._listeners[evt],
-                    callback.filter(val => {
-                        return (typeof val === "function");
-                    }));
+                for (let listener of callback) {
+                    this._onEvtCallback(evt, listener);
+                }
             }
             
             return this;
         }
         off(...args) {
-            
+            if (args.length === 1) {
+                return this._offJSON.apply(this, args);
+            }
+            if (args.length >= 2) {
+                return this._offEvtCallback.apply(this, args);
+            }
+            return this;
         }
         _offJSON(json) {
+            if (typeof json !== "object") {
+                return this;
+            }
             
+            for (let evt in json) {
+                if (json.hasOwnProperty(evt)) {
+                    this._offEvtCallback(evt, json[evt]);
+                }
+            }
+            
+            return this;
         }
-        _offEvtCallback() {
+        _offEvtCallback(evt, callback) {
+            if (this._listeners[evt] === undefined) {
+                return this;
+            }
             
+            if (typeof callback === "function") {
+                if (!callback[CALLBACK_UID_KEY]) {
+                    return this;
+                }
+                let callbackUid = callback[CALLBACK_UID_KEY];
+                let filtered = this._listeners[evt].filter(val => {
+                    return (val[CALLBACK_UID_KEY] !== callbackUid);
+                });
+                
+                if (!filtered.length) {
+                    delete this._listeners[evt];
+                } else {
+                    this._listeners[evt] = filtered;
+                }
+            } else if (callback.constructor === Array) {
+                for (let listener of callback) {
+                    this._offEvtCallback(evt, listener);
+                }
+            }
+            
+            return this;
         }
         fire(evt, data = {}) {
             if (typeof this.listeners[evt] !== "undefined") {
