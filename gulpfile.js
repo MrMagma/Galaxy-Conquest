ea7a70275d1910d4bb378ff66f868e9435b4cfe3
Mr. Magma <joshuamagma2013@gmail.com>
2015-12-07 21:43:02 -0600

    Fix broken build process

--- gulpfile.js
+++ gulpfile.js
@@ -5,35 +5,36 @@ var babel = require("gulp-babel");
 var browserify = require("gulp-browserify");
 var jshint = require("gulp-jshint");
 var spawnMocha = require("gulp-spawn-mocha");
+var del = require("del");
 
 var builds = {
     "transform-mars-src": function() {
-        return gulp.src(["Mars/src/**/*.js"])
+        return gulp.src(["./Mars/src/**/*.js"])
             .pipe(jshint({
                 
             }))
             .pipe(babel())
-            .pipe(gulp.dest("Mars/build/"))
+            .pipe(gulp.dest("./Mars/build/"));
     },
     "build-mars-src": function() {
-        return gulp.src(["Mars/build/MarsEngine.js"])
+        return gulp.src(["./Mars/build/MarsEngine.js"])
             .pipe(browserify())
-            .pipe(gulp.dest("build/"))
+            .pipe(gulp.dest("./build/"));
     },
     "build-mars": ["transform-mars-src", "build-mars-src"],
     "transform-game-src": function() {
-        return gulp.src(["Game/src/**/*.js"])
+        return gulp.src(["./Game/src/**/*.js"])
             .pipe(jshint({
                 
             }))
             .pipe(jshint.reporter("default"))
             .pipe(babel())
-            .pipe(gulp.dest("Game/build/"))        
+            .pipe(gulp.dest("./Game/build/"));
     },
     "build-game-src": function() {
-        return gulp.src(["Game/build/Game.js"])
+        return gulp.src(["./Game/build/Game.js"])
             .pipe(browserify())
-            .pipe(gulp.dest("build/"))        
+            .pipe(gulp.dest("./build/"));   
     },
     "build-game": ["transform-game-src", "build-game-src"]
 }
@@ -42,19 +43,20 @@ var buildKeys = [];
 
 for (var buildKey in builds) {
     if (builds.hasOwnProperty(buildKey)) {
-        buildKeys.push(buildKey);
-        if (builds[buildKey].constructor === Array) {
-            gulp.task(buildKey, builds[buildKey]);
-        } else {
-            gulp.task(buildKey, builds[buildKey]);
-        }
+        gulp.task(buildKey, builds[buildKey]);
     }
 }
 
-gulp.task("build", buildKeys);
+gulp.task("clear-builds", function() {
+    del("./Game/build/*");
+    del("./Mars/build/*");
+    return del.sync("./build/*");
+});
+
+gulp.task("build", ["build-mars", "build-game"]);
 
 gulp.task("test", function() {
-    return gulp.src(["test/**/*.js"])
+    return gulp.src(["./test/**/*.js"])
         .pipe(spawnMocha())
 });
 
