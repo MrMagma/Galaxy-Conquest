65468489ac7edc3e74fcc52c16317f9f0728632b
Mr. Magma <joshuamagma2013@gmail.com>
2016-01-05 16:58:26 -0600

    Work on Units

--- Mars/src/Unit.js
+++ Mars/src/Unit.js
@@ -2,12 +2,50 @@ var Unit = (function() {
     
     let MarsObject = require("MarsObject");
     
-    class Unit extends MarsObject {
-        constructor(type) {
-            super();
-            this.type = type;
+    let UnitTypes = {
+        Base: class Base extends MarsObject {
+            constructor(cfg) {
+                super(cfg);
+                this.type = "base";
+            }
         }
-    }
+    };
+    
+    let ignoreKeys = ["type", "base", "init"];
+    
+    let Unit = {
+        define(data = {}) {
+            let {type, base = "Base", init = () => {}} = data;
+            
+            if (!UnitTypes.hasOwnProperty(base)) {
+                return;
+            }
+            
+            class Unit extends unitTypes[base] {
+                constructor(cfg) {
+                    super(cfg);
+                    this.type = type;
+                    init.call(this, cfg);
+                }
+            }
+            
+            for (let key in data) {
+                if (data.hasOwnProperty(key) &&
+                    ignoreKeys.indexOf(key) === -1) {
+                    Unit.prototype[key] = data[key];
+                }
+            }
+            
+            UnitTypes[type] = Unit;
+            
+            return Unit;
+        },
+        create(type, data) {
+            if (unitTypes.hasOwnProperty(type)) {
+                return new UnitTypes[type](data);
+            }
+        }
+    };
     
     module.exports = Unit;
     return Unit;    
